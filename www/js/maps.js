2e5be34ebecf7a23d8ac1e0f303f47f5d87d4298
Andre Christoga <andrechristoga@gmail.com>
2016-10-08 17:43:20 +0700

    Fuck off.

--- www/js/maps.js
+++ www/js/maps.js
@@ -23,56 +23,38 @@ function sendData(latitude, longtitude) {
 	alert("Laporan anda sudah terkirim");
 }
 
-var map = new google.maps.Map(document.getElementById('map'), {
-	center: {lat: -34.397, lng: 150.644},
-	zoom: 6
-});
-var infoWindow = new google.maps.InfoWindow({map: map});
-navigator.geolocation.getCurrentPosition(function(position) {
-	var pos = {
-		lat: position.coords.latitude,
-		lng: position.coords.longitude
-	};
-	
-	sendData(pos.lat, post.lng);
+navigator.geolocation.getCurrentPosition(geolocationSuccess,
+                                         [geolocationError],
+                                         [geolocationOptions]);
+
+function action() {	
+	'use strict';
+//		var onSucces = function(position){
+//			var latitude = position.coords.latitude,
+//					longtitude = position.coords.longitude;
+//		}
+//		sendData(pos.lat, post.lng);
+		// onSuccess Callback
+// This method accepts a Position object, which contains the
+// current GPS coordinates
+//
+var onSuccess = function(position) {
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+};
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
 }
 
-
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-
-//function initMap() {
-//	var map = new google.maps.Map(document.getElementById('map'), {
-//		center: {lat: -34.397, lng: 150.644},
-//		zoom: 6
-//	});
-//	var infoWindow = new google.maps.InfoWindow({map: map});
-//
-//	// Try HTML5 geolocation.
-//	if (navigator.geolocation) {
-//		navigator.geolocation.getCurrentPosition(function(position) {
-//			var pos = {
-//				lat: position.coords.latitude,
-//				lng: position.coords.longitude
-//			};
-//
-//			infoWindow.setPosition(pos);
-//			infoWindow.setContent('Location found.');
-//			map.setCenter(pos);
-//		}, function() {
-//			handleLocationError(true, infoWindow, map.getCenter());
-//		});
-//	} else {
-//		// Browser doesn't support Geolocation
-//		handleLocationError(false, infoWindow, map.getCenter());
-//	}
-//}
-//
-//function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//	infoWindow.setPosition(pos);
-//	infoWindow.setContent(browserHasGeolocation ?
-//												'Error: The Geolocation service failed.' :
-//												'Error: Your browser doesn\'t support geolocation.');
-}
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	}
