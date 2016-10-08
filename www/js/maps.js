c2fb83c57291b177f7a8fd50c3a90daed6c95fa8
Andre Christoga <andrechristoga@gmail.com>
2016-10-08 17:50:13 +0700

    It is done. (Not really)

--- www/js/maps.js
+++ www/js/maps.js
@@ -1,60 +1,33 @@
-'use strict';
-
-// Firebase Config
-var config = {
-  apiKey: "fFRaVtFRskcRYpFdbbLC6NaGz8JSEpGPggQrudgF",
-  authDomain: "cepatsembuh.firebaseapp.com",
-  databaseURL: "https://cepatsembuh.firebaseio.com",
-};
-firebase.initializeApp(config);
-
-var ref = firebase.database().ref();
-var faskes = ref.child('rs-tni');
-var nama_faskes = faskes.child('contoh');
-var panic = faskes.child('darurat')
-var pasien = faskes.child('pasien');
-
-function sendData(latitude, longtitude) {
-	panic.push().set({
-		latitude: latitude,
-		longtitude: longtitude
-	})
-	
-	alert("Laporan anda sudah terkirim");
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+	console.log("navigator.geolocation works well");
 }
 
-navigator.geolocation.getCurrentPosition(geolocationSuccess,
-                                         [geolocationError],
-                                         [geolocationOptions]);
-
-function action() {	
-	'use strict';
-//		var onSucces = function(position){
-//			var latitude = position.coords.latitude,
-//					longtitude = position.coords.longitude;
-//		}
-//		sendData(pos.lat, post.lng);
-		// onSuccess Callback
-// This method accepts a Position object, which contains the
-// current GPS coordinates
-//
 var onSuccess = function(position) {
-    alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
+  var latitude = position.coords.latitude,
+			longitude = position.coords.longitude;
+	
+	var http = new XMLHttpRequest();
+	var url = "";
+	var params = "latitude=" + latitude + "longitude=" + longitude;
+	http.open("POST", url, true);
+
+	//Send the proper header information along with the request
+	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+	http.onreadystatechange = function() {//Call a function when the state changes.
+			if(http.readyState == 4 && http.status == 200) {
+					alert(http.responseText);
+			}
+	}
+	http.send(params);
 };
 
 // onError Callback receives a PositionError object
 //
 function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
+	alert('code: '    + error.code    + '\n' +
+				'message: ' + error.message + '\n');
 }
 
 navigator.geolocation.getCurrentPosition(onSuccess, onError);
-	}
